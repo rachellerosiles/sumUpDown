036151cee0f528edf0905d134a3c416fc971e255
Rachelle <rrosiles@hawk.iit.edu>
2024-02-01 18:20:45 -0600

    Initial Commit

---
+++ sumUpDown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sumUpDown
+//
+//  Created by PHYS 440 Rachelle on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sumUpDown/sumUpDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  sumUpDownApp.swift
+//  sumUpDown
+//
+//  Created by PHYS 440 Rachelle on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct sumUpDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
